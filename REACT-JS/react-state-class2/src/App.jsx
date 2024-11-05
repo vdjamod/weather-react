c1baeb4110076ba5d28e410b7639aa7694b2e793
vipuljamod <vipuljamod122@gamil.com>
2024-11-05 15:44:47 +0530

    Todo

---
+++ REACT-JS/react-state-class2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LudoBoard from './LudoBoard';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <>
+      {/* <LudoBoard /> */}
+      <TodoList />
+
+    </>
+  )
+}
+
+export default App
